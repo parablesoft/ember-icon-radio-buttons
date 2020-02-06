3a9d6db8ce8bbb4de4f2570c739ebfa6198289db
Vic Amuso <vic@parablesoft.com>
2020-02-06 15:40:52 -0500

    Fix for font-awesome config

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,6 +5,10 @@ var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 module.exports = function(defaults) {
   var app = new EmberAddon(defaults, {
     // Add options here
+    'ember-font-awesome': {
+      useScss: true,
+      fontsOutput: "/assets/fonts"
+    }
   });
 
   /*
